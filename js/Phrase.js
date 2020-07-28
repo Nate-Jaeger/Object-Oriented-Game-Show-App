12b7d0eedc2193a67194cde6bb1cd9e23a0feb58
Nate Jaeger <Nate.RJS21@gmail.com>
2020-07-27 22:53:00 -0500

    Reformatted li.setAttribute()

--- js/Phrase.js
+++ js/Phrase.js
@@ -11,16 +11,13 @@ class Phrase {
 
       //Loop over phrase string as an array
       for (const letter of this.phrase) {
-         //Create, append, and set text for phrase LI's
+         //Create, append, and set text for letter LI's
          let li = document.createElement("li");
          li.textContent = letter;
          phraseUL.appendChild(li);
 
          //Assign different classes based on if the letter is a space or not
-         li.setAttribute(
-            "class",
-            letter !== " " ? `hide letter ${letter}` : "space",
-         );
+         li.setAttribute("class", letter !== " " ? `hide letter ${letter}` : "space");
       }
    }
 }
