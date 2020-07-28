0c25a69b144a37454248120c435cf365124158d2
Nate Jaeger <Nate.RJS21@gmail.com>
2020-07-27 20:24:56 -0500

    Made createPhrases method
    createPhrases with fill an array with 5 Phrase objects, and return the array

--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,22 @@
 class Game {
     constructor(){
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = this.createPhrases();
         this.activePhrase = null;
     }
+
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases(){
+        const phrases = [];
+        phrases.push(new Phrase("Life is like a box of chocolates"));
+        phrases.push(new Phrase("Carpe Diem"));
+        phrases.push(new Phrase("The stuff that dreams are made of"));
+        phrases.push(new Phrase("Just keep swimming"));
+        phrases.push(new Phrase("Rosebud"));
+
+        return phrases;
+    };
 }
