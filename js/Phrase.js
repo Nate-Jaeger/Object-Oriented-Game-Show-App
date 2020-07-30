6973e585d04f05d47b3dc9bb3024471d653a633a
Nate Jaeger <Nate.RJS21@gmail.com>
2020-07-29 19:16:43 -0500

    Created 2 methods for Phrase class
    Created chekcLetter method and showMatchedLetter method

--- js/Phrase.js
+++ js/Phrase.js
@@ -19,5 +19,26 @@ class Phrase {
          //Assign different classes based on if the letter is a space or not
          li.setAttribute("class", letter !== " " ? `hide letter ${letter}` : "space");
       }
-   }
+	}
+	
+	/**
+	* Checks if passed letter is in phrase
+	* @param (string) letter - Letter to check
+	*/
+	checkLetter(letter){
+		return this.phrase.includes(letter);
+	};
+
+	/**
+	* Displays passed letter on screen after a match is found
+	* @param (string) letter - Letter to display
+	*/
+	showMatchedLetter(letter){
+		let letters = document.getElementsByClassName(letter);
+		
+		for (const letter of letters) {
+			letter.classList.remove('hide');
+			letter.classList.add('show');
+		}
+	};
 }
